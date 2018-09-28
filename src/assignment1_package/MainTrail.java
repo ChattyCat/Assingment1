1eae02fd0ad387faa0f7eced178f480ddeae0ab1
Johan <dotjohan@gmail.com>
2018-09-28 18:15:39 +0200

    Initial commit

---
+++ src/assignment1_package/MainTrail.java
@@ -0,0 +1,13 @@
+package assignment1_package;
+
+public class MainTrail {
+
+	public static void main(String[] args) {
+		Register reg = new Register();
+		reg.retrieveAllStuReg();
+		String sID = "S1";
+		System.out.println(reg.retrieveStuReg( sID).getsName());
+
+	}
+
+}
